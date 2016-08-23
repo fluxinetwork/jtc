57e012eb709371e6e1f4e4e96ee3634e53dc80a6
Rolland Yann <rollandyann@gmail.com>
2016-08-23 11:49:21 +0200

    Add under-construction
    under-construction.php

---
+++ under-construction.php
@@ -0,0 +1,41 @@
+<?php //get_header(); ?>
+	<section>
+		<header>
+			<h1>..._ Site en construction _...</h1>
+		</header>
+
+		<article>
+			<h3>La 3ème édition de la Journée de la transition aura lieu le 24 septembre 2016</h3>
+
+			<p>En attendant vous pouvez retrouver toute notre actualité<br>
+			sur le site du <a href="http://www.transitioncitoyenne.org/">Collectif pour une Transition Citoyenne</a>.</p>
+		</article>
+
+	</section>
+
+	
+<style type="text/css">
+	body{
+		width: 100%;
+		height: 100%;
+		background: #e5442f;
+		color: #fff;
+		margin: 0;
+		padding: 0;
+		font-family: "Open sans", sans-serif;
+	}
+
+	section{
+		text-align: center;
+		padding: 50px 0;
+		max-width: 800px;
+		margin: 0 auto;
+	}
+	
+	a{
+		color: #ffd52e;
+		font-weight: bold;
+		text-decoration: underline;
+	}
+</style>
+<?php //get_footer(); ?>
