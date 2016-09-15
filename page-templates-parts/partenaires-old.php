37340c4430b43f87b5594061e9c3545b16f25219
Thibaut Caroli <thibaut.caroli.pro@gmail.com>
2016-09-15 12:18:32 +0200

    Motif partenaires

--- page-templates-parts/partenaires-old.php
+++ page-templates-parts/partenaires-old.php
@@ -19,8 +19,12 @@
 	<div class="wrap-content">
 		<h2>Ils nous soutiennent</h2>
 		<ul class="logos">
+			<li class="logos__logo--small"><a href="http://www.generations-futures.fr/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-generations-futures.jpg"></a></li>
+			<li class="logos__logo--small"><a href="http://www.fnab.org/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-fnab.jpg"></a></li>
 			<li class="logos__logo--small"><a href="http://lesincroyablescomestibles.fr/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-comestible.jpg"></a></li>
+			<li class="logos__logo--small"><a href="http://www.rac-f.org" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-rac.jpg"></a></li>
 			<li class="logos__logo--small"><a href="http://bioetlocalcestlideal.org/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-mager-bio.jpg"></a></li>
+			<li class="logos__logo--small"><a href="http://www.agirpourlenvironnement.org/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-ape.jpg"></a></li>
 			<li class="logos__logo--small"><a href="http://www.reseau-coherence.org/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-coherence.jpg"></a></li>
 			<li class="logos__logo--small"><a href="http://www.colibris-lemouvement.org/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-colibris.jpg"></a></li>
 			<li class="logos__logo--small"><a href="http://www.biocoop.fr/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-biocoop.jpg"></a></li>
