62aab047a0616d1b0d010845d2f36f1b4b3b57a4
Thibaut Caroli <thibaut.caroli@gmail.com>
2016-09-16 15:23:05 +0200

    HelloAsso

--- page-templates-parts/partenaires.php
+++ page-templates-parts/partenaires.php
@@ -4,27 +4,40 @@
  */
 ?>
 
-<aside class="section">
-	<div class="wrap-content">
-		<h2>Nos soutiens financiers</h2>
-		<ul class="logos">
-			<li class="logos__logo"><a href="http://www.fph.ch/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/fondation-charles-leopold.jpg" alt="Fondation Charles Leopold Mayer"></a></li>
-			<li class="logos__logo"><a href="http://www.fondationdefrance.org/fondation/fondation-un-monde-par-tous" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/fondation-monde-par-tous.jpg" alt="Fondation un monde par tous"></a></li>
-			<li class="logos__logo"><a href="http://www.up-group-foundation.com/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/fondation-up.jpg" alt="Fondation Up"></a></li>
-		</ul>
+<aside>
+	<div class="section">
+		<div class="wrap-content">
+			<h2>Nos soutiens financiers</h2>
+			<ul class="logos">
+				<li class="logos__logo"><a href="http://www.fph.ch/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/fondation-charles-leopold.jpg" alt="Fondation Charles Leopold Mayer"></a></li>
+				<li class="logos__logo"><a href="http://www.fondationdefrance.org/fondation/fondation-un-monde-par-tous" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/fondation-monde-par-tous.jpg" alt="Fondation un monde par tous"></a></li>
+				<li class="logos__logo"><a href="http://www.up-group-foundation.com/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/fondation-up.jpg" alt="Fondation Up"></a></li>
+			</ul>
+		</div>
 	</div>
-</aside>
+	
+	<div class="section">
+		<div class="wrap-content">
+			<h2>Envie d'agir ?</h2>
+			<h3>Soutenez-nous sur la plateforme HelloAsso</h3><br>
+			<a href="https://www.helloasso.com/associations/collectif-pour-une-transition-citoyenne/collectes/collectif-pour-une-transition-citoyenne" class="button--cta h3">Par ici !</a>
+			<ul class="logos">
+				<li class="logos__logo"><a href="https://www.helloasso.com/associations/collectif-pour-une-transition-citoyenne/collectes/collectif-pour-une-transition-citoyenne" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-helloasso.jpg" alt="Hello Asso"></a></li>
+			</ul>
+		</div>
+	</div>
 
-<aside class="section">
-	<div class="wrap-content">
-		<h2>Ils nous soutiennent</h2>
-		<ul class="logos">
-			<li class="logos__logo--small"><a href="http://lesincroyablescomestibles.fr/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-comestible.jpg"></a></li>
-			<li class="logos__logo--small"><a href="http://bioetlocalcestlideal.org/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-mager-bio.jpg"></a></li>
-			<li class="logos__logo--small"><a href="http://www.reseau-coherence.org/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-coherence.jpg"></a></li>
-			<li class="logos__logo--small"><a href="http://www.colibris-lemouvement.org/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-colibris.jpg"></a></li>
-			<li class="logos__logo--small"><a href="http://www.biocoop.fr/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-biocoop.jpg"></a></li>
-		</ul>
+	<div class="section">
+		<div class="wrap-content">
+			<h2>Ils nous soutiennent</h2>
+			<ul class="logos">
+				<li class="logos__logo--small"><a href="http://lesincroyablescomestibles.fr/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-comestible.jpg"></a></li>
+				<li class="logos__logo--small"><a href="http://bioetlocalcestlideal.org/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-mager-bio.jpg"></a></li>
+				<li class="logos__logo--small"><a href="http://www.reseau-coherence.org/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-coherence.jpg"></a></li>
+				<li class="logos__logo--small"><a href="http://www.colibris-lemouvement.org/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-colibris.jpg"></a></li>
+				<li class="logos__logo--small"><a href="http://www.biocoop.fr/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/app/img/logos/logo-partenaire-biocoop.jpg"></a></li>
+			</ul>
+		</div>
 	</div>
 </aside>
 
